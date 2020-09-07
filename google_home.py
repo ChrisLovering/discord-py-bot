b4b17c379af065246f2c6fff0afa2ff663a08011
Chris Lovering <chris.lovering.95@gmail.com>
2020-09-07 22:50:32 +0100

    OOP google home implementation

--- google_home.py
+++ google_home.py
@@ -1,43 +1,54 @@
 import pychromecast
-from gtts import gTTS
+from gtts import gTTS, lang
 from slugify import slugify
 from pathlib import Path
 from urllib.parse import urlparse
 import socket
 
-chromecast_name = "Living Room speaker"
+class GoogleHome():
+    
+    def __init__(self):
+        self.chromecast_name = "Living Room speaker"
+        self.languages = self.get_langs()
+        self.dirname = Path(__file__).parent.absolute()
+        Path(self.dirname, 'static', 'cache').mkdir(parents=True, exist_ok=True)
 
-dirname = Path(__file__).parent.absolute()
-Path(dirname, 'static', 'cache').mkdir(parents=True, exist_ok=True)
+        self.chromecasts = pychromecast.get_chromecasts()
+        self.cast = next(cc for cc in self.chromecasts[0] if cc.device.friendly_name == self.chromecast_name)
 
-chromecasts = pychromecast.get_chromecasts()
-cast = next(cc for cc in chromecasts[0] if cc.device.friendly_name == chromecast_name)
+    @staticmethod
+    def get_ip():
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            # doesn't even have to be reachable
+            s.connect(('10.255.255.255', 1))
+            IP = s.getsockname()[0]
+        except Exception:
+            IP = '127.0.0.1'
+        finally:
+            s.close()
+        return IP
 
-def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.255.255.255', 1))
-        IP = s.getsockname()[0]
-    except Exception:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP
+    @staticmethod
+    def get_langs():
+        return lang.tts_langs()
 
-def play_tts(text, lang='en', slow=False):
-    tts = gTTS(text=text, lang=lang, slow=slow)
-    filename = slugify(text+"-"+lang+"-"+str(slow)) + ".mp3"
-    tts_file = Path(dirname, 'static', 'cache', filename)
-    if not tts_file.is_file():
-        tts.save(tts_file)
+    def play_tts(self, text, lang='en', slow=False):
+        tts = gTTS(text=text, lang=lang, slow=slow)
+        filename = slugify(text+"-"+lang+"-"+str(slow)) + ".mp3"
+        tts_file = Path(self.dirname, 'static', 'cache', filename)
+        if not tts_file.is_file():
+            tts.save(tts_file)
 
-    mp3_url = "http://" + get_ip() + ':5000/static/cache/' + filename 
-    play_mp3(mp3_url)
+        mp3_url = "http://" + self.get_ip() + ':5000/static/cache/' + filename 
+        self.play_mp3(mp3_url)
 
 
-def play_mp3(mp3_url):
-    cast.wait()
-    mc = cast.media_controller
-    mc.play_media(mp3_url, 'audio/mp3')
-    mc.block_until_active()
+    def play_mp3(self, mp3_url):
+        self.cast.wait()
+        mc = self.cast.media_controller
+        mc.play_media(mp3_url, 'audio/mp3')
+        mc.block_until_active()
+
+if __name__ == "__main__":
+    print(GoogleHome.get_langs())
