34e3634ae6ecc94509f6144efe5109d5a382aa00
Chris Lovering <chris.lovering.95@gmail.com>
2020-09-07 22:51:09 +0100

    use OOP google home and language support

--- main_bot.py
+++ main_bot.py
@@ -1,12 +1,23 @@
 import os
 import aiohttp
 import discord
+import pickle
+from pathlib import Path
 from discord.ext import commands
 from urllib.parse import urlparse
-import google_home
+from google_home import GoogleHome
+
+dirname = Path(__file__).parent.absolute()
+pickle_file = Path(dirname, 'user_langs.pkl')
+if pickle_file.is_file():
+    with open(pickle_file, 'rb') as f:
+        user_langs = pickle.load(f)
+else:
+    user_langs = {}    
 
 bot = commands.Bot(command_prefix="!", help_command=None)
 debug = False
+home_obj = GoogleHome()
 
 async def get_ngrok_ip():
     async with aiohttp.ClientSession() as session:
@@ -28,6 +39,12 @@ async def on_message(message):
         print(f"{message.author} sent '{message.content}' in {message.channel}")
     await bot.process_commands(message)
 
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        return
+    raise error
+
 # Top level command, this is so that this bot doesn't overlap with others that use !
 @bot.group()
 async def alfred(ctx):
@@ -41,15 +58,43 @@ async def ip(ctx):
     await ctx.send(ip)
 
 @alfred.group()
+async def lang(ctx):
+    if ctx.invoked_subcommand is None:
+        await ctx.send("Only subcommands are get, set or list")
+
+@lang.command(name='list')
+async def lang_list(ctx):
+    home_obj.languages = home_obj.languages
+    languages_list = [f'{code}: {lang}' for code, lang in home_obj.languages.items()]
+    await ctx.author.send('\n'.join(['Supported languages are:']+languages_list))
+    await ctx.send("DM'd you the list as its quite long")
+
+@lang.command(name='get')
+async def lang_get(ctx):
+    lang = user_langs.get(ctx.author.id, "en")
+    await ctx.send(f'Your current language is {lang}-{home_obj.languages[lang]}')
+
+@lang.command(name='set')
+async def lang_set(ctx, lang):
+    if lang in home_obj.languages:
+        user_langs[ctx.author.id] = lang
+        message = f'Set {ctx.author}s language to {lang}-{home_obj.languages[lang]}'
+    else:
+        message = f'Language {lang} could not be found. Do !alfred lang list for a list of supported languages'
+    await ctx.send(message)
+
+@alfred.group()
 async def say(ctx, *, message='test'):
     if ctx.invoked_subcommand is None:
-        google_home.play_tts(message)
-        await ctx.send(f'Now playing {message}')
+        lang = user_langs.get(ctx.author.id, "en")
+        home_obj.play_tts(message, lang=lang)
+        await ctx.send(f'Now playing {message} in {home_obj.languages[lang]}')
 
 @say.command(name='slow')
 async def say_slow(ctx, *, message='test'):
-    google_home.play_tts(message, slow=True)
-    await ctx.send(f'Now playing {message}')
+    lang = user_langs.get(ctx.author.id, "en")
+    home_obj.play_tts(message, lang=lang, slow=True)
+    await ctx.send(f'Now playing {message} slowly, in {home_obj.languages[lang]}')
 
 # Sends a dm to the user
 @alfred.group()
@@ -63,4 +108,7 @@ async def dm_ip(ctx, user: discord.User, *, message=None):
     message = ' '.join([await get_ngrok_ip(), message])
     await user.send(message)
 
+
 bot.run(os.environ["DISCORD_BOT_TOKEN"])
+with open(pickle_file, 'wb') as f:
+    pickle.dump(user_langs, f)
