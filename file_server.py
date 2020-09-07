bad183b257bf5c339440689c9478c7467dbf7588
Chris Lovering <chris.lovering.95@gmail.com>
2020-09-07 19:43:34 +0100

    remove file traversal vuln

--- file_server.py
+++ file_server.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, send_from_directory
 app = Flask(__name__)
 
-@app.route('/static/<path:path>')
-def send_static(path):
-    return send_from_directory('static', path)
+@app.route('/static/<filename>')
+def send_static(filename):
+    return send_from_directory('static/cache', filename=filename)
 
 if __name__ == '__main__':
         app.run(debug=True,host='0.0.0.0')
