564cfa7c8cf2f4c6d29119bf8a477a29f7bd7988
Chris Lovering <chris.lovering.95@gmail.com>
2020-09-07 19:56:02 +0100

    Fix indents

--- file_server.py
+++ file_server.py
@@ -6,4 +6,4 @@ def send_static(filename):
     return send_from_directory('static/cache', filename=filename)
 
 if __name__ == '__main__':
-        app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0')
